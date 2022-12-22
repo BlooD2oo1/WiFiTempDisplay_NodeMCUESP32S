1f318e714694cf0a9872437f253ebf75876a7a9d
BlooD2oo1 <blod2o1@gmail.com>
2022-12-22 22:16:01 +0100

    change temp sensor IDs from "kornye kazan" to new IDs.

--- WiFiTempDisplay/MainTask.h
+++ WiFiTempDisplay/MainTask.h
@@ -27,8 +27,8 @@ private:
   const uint8_t     m_iOneWireBus = 13;
   OneWire           m_OneWire;
   DallasTemperature m_Sensors;
-  const uint8_t     m_piSensorDeviceAddress[SENSORCOUNT][8] = { { 0x28, 0x22, 0x43, 0x97, 0x94, 0x07, 0x03, 0x3C },
-                                                                { 0x28, 0x12, 0x56, 0xE8, 0x5C, 0x21, 0x01, 0xEC } };
+  const uint8_t     m_piSensorDeviceAddress[SENSORCOUNT][8] = { { 0x28, 0x44, 0xF3, 0xD0, 0x5C, 0x21, 0x01, 0xC5 },
+                                                                { 0x28, 0x4C, 0x83, 0xD0, 0x5C, 0x21, 0x01, 0x09 } };
   int16_t           m_iSensorTemperature[SENSORCOUNT] = {DEVICE_DISCONNECTED_RAW,DEVICE_DISCONNECTED_RAW};
 
   U8G2_SH1106_128X64_NONAME_F_HW_I2C  m_u8g2;
